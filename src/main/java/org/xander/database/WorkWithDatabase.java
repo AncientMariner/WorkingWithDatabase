aa183b52a1fc0906a532b827a71d291f2cfe3397
ale <ale@ale.(none)>
2013-06-14 10:29:05 +0300

    create tables and insert data into

--- src/main/java/org/xander/database/WorkWithDatabase.java
+++ src/main/java/org/xander/database/WorkWithDatabase.java
@@ -7,50 +7,41 @@ import java.sql.Statement;
 public class WorkWithDatabase {
     private Connection connection;
 
+    String createTable = "CREATE TABLE groups (id INT AUTO INCREMENT, " +
+                                                    "name VARCHAR(20), PRIMARY KEY (id)) " +
+                                                    "ENGINE=InnoDB DEFAULT CHARSET=utf8";
+    String createAnotherTable = "CREATE TABLE products(id INT(5) AUTO_INCREMENT, " +
+                                                    "name VARCHAR(20), " +
+                                                    "description VARCHAR(30), " +
+                                                    "groups_id INT, " +
+                                                    "PRIMARY KEY (id)," +
+                                                    "FOREIGN KEY (groups_id) REFERENCES groups(id) ON DELETE CASCADE ON UPDATE CASCADE)" +
+                                                    "ENGINE=InnoDB DEFAULT CHARSET=utf8;";
+
+    String insertIntoGroups1 = "INSERT INTO groups (name) VALUES ('PC');";
+    String insertIntoGroups2 = "INSERT INTO groups (name) VALUES ('HDD');";
+    String insertIntoGroups3 = "INSERT INTO groups (name) VALUES ('Monitor');";
+
+    String insertIntoProducts1 = "INSERT INTO products (name, description, groups_id) VALUES ('Macintosh', 'expensive', 1);";
+    String insertIntoProducts11 = "INSERT INTO products (name, description, groups_id) VALUES ('Dell', 'powerful', 1);";
+
+    String insertIntoProducts2 = "INSERT INTO products (name, description, groups_id) VALUES ('WD', 'medium and powerful', 2);";
+    String insertIntoProducts22 = "INSERT INTO products (name, description, groups_id) VALUES ('SeaGate', 'powerful', 2);";
+
+    String insertIntoProducts3 = "INSERT INTO products (name, description, groups_id) VALUES ('LG', 'brilliant', 3);";
+    String insertIntoProducts33 = "INSERT INTO products (name, description, groups_id) VALUES ('Samsung', 'good', 3);";
+
+
     public WorkWithDatabase(Connection connection) {
         this.connection = connection;
     }
 
-    public void createTable() throws SQLException {
+    public void executeQuery() throws SQLException {
         Statement statement = null;
-        String createTableSQL = "CREATE TABLE GROUP("
-                + "GROUP_ID INT NOT NULL,"
-                + "GROUP_NAME VARCHAR(20),"
-                + "PRIMARY KEY (GROUP_ID)"
-                + ")";
-
         try {
             statement = connection.createStatement();
-            statement.execute(createTableSQL);
-
-            System.out.println("Table \"GROUP\" is created!");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        } finally {
-            if (statement != null)
-                statement.close();
-            if (connection != null)
-                connection.close();
-        }
-    }
-
-    public void createAnotherTable() throws SQLException {
-        Statement statement = null;
-        String createTableSQL = "DROP TABLE IF EXISTS PRODUCT;"
-                + "CREATE TABLE PRODUCT("
-                + "PRODUCT_ID INT(5) NOT NULL, "
-                + "PRODUCT_NAME VARCHAR(20), "
-                + "PRODUCT_DESCRIPTION VARCHAR(30), "
-                + "CONSTRAINT FK_GROUP_ID FOREIGN KEY (GROUP_ID)" +
-                  "REFERENCES GROUP (GROUP_ID) ON DELETE CASCADE ON UPDATE CASCADE,"
-                + "PRIMARY KEY (PRODUCT_ID) "
-                + ") ENGINE=InnoDB DEFAULT CHARSET=utf8";
-
-        try {
-            statement = connection.createStatement();
-            statement.execute(createTableSQL);
-
-            System.out.println("Table \"Product\" is created!");
+            statement.execute(insertIntoProducts33);
+            System.out.println("Query was executed");
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         } finally {
